2f1e5584c81afd1be0fedc794772f42f86512cf8
rajeshvree karma <rajehvsari.karma.ets@gmail.com>
2021-03-16 19:43:58 +0530

    updated undefined element problem & made it responsive

--- differ.js
+++ differ.js
@@ -12,8 +12,8 @@ jQuery(document).ready(function(){
         var patt1 = /\n/;
         let data1 = jQuery("#original_text").val();
         let data2 = jQuery("#comp_text").val();
-        data2 = data2.trim();
-        data1 = data1.trim();
+        // data2 = data2.trim();
+        // data1 = data1.trim();
         data1 = data1.split(patt1);
         data2 = data2.split(patt1);
         // let d1v = data1.val();
@@ -24,18 +24,22 @@ jQuery(document).ready(function(){
         $("#result_original_text").html("");
         $("#result_comp_text").html("");
         let len;
-
+        let extra;
         if(data1.length>data2.length){
-            len=data1.length;
+            len=data2.length;
+            extra=data1.length-data2.length;
+            
         }
         else{
-            len=data2.length;
+            len=data1.length;
+            extra=data2.length-data1.length;
         }
+
 
         for(let i=0; i<len; i++){
             
             if(data1[i]==data2[i]){
-                $("#result_original_text").append("<span>" + data1[i] + "</span>" + "<br>");
+                $("#result_original_text").append(`<span>${data1[i]}</span><br>`);
                 $("#result_comp_text").append("<span>" + data2[i] + "</span>" + "<br>");
                 // $("#result_original_text").css({"background-color":"green", "color":"white"});
                 // $("#result_comp_text").css({"background-color":"green", "color":"white"});
@@ -46,6 +50,21 @@ jQuery(document).ready(function(){
                 // $("#result_original_text").css({"background-color":"yellow", "color":"black"});
                 // $("#result_comp_text").css({"background-color":"red", "color":"white"});
             }
-        }  
+        } 
+        
+        if(extra){
+            extra+=len;
+                if(data1.length>data2.length){
+                    for(i=len;i<extra;i++) {
+                        $("#result_original_text").append("<em>" + data1[i] + "</em>" + "<br>");
+                    }
+                }
+                else{
+                    for(i=len;i<extra;i++) {
+                        $("#result_comp_text").append("<em>" + data2[i] + "</em>" + "<br>"); 
+                    }
+                }
+        }
+
     })
 });
